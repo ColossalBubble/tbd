2cceed702c072c38099d51fc8b237f2b4daf8477
Greg Roche <grroche.us@gmail.com>
2016-08-11 12:17:40 -0700

    Make spaces consistent in injections

--- client/src/containers/LandingPage.jsx
+++ client/src/containers/LandingPage.jsx
@@ -6,9 +6,9 @@ class LandingPage extends Component {
     return (
       <div id='roche'>
         WELCOME TO TBD! <br />
-        <button onClick={() => { this.props.change('selectInstrument')}}>Join Room </button><br />
+        <button onClick={() => { this.props.change('selectInstrument') }}>Join Room </button><br />
         <input type="text" /><br />
-        <button onClick={() => { this.props.change('selectInstrument')}}>Create Room </button><br />
+        <button onClick={() => { this.props.change('selectInstrument') }}>Create Room </button><br />
       </div>
     );
   }
