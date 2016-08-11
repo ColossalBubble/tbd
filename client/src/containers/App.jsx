8975f77413c9251eb6387fb90bd1d0c550435b68
Greg Roche <grroche.us@gmail.com>
2016-08-11 14:45:30 -0700

    Refactor render of App to have one return statement

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -9,94 +9,78 @@ import JamRoom from './JamRoom';
 
 class App extends Component {
   constructor(props) {
-    super(props)
-    this.toggleRoom = this.toggleRoom.bind(this);
-    this.toggleView = this.toggleView.bind(this);
-    this.toggleInstrument=this.toggleInstrument.bind(this);
-
     super(props);
+
     this.state = {
       view: "LandingPage",
       instrument: "start",
       roomType: "start",
-      roomId: "start"
+      roomId: "start",
     };
+
+    this.toggleRoom = this.toggleRoom.bind(this);
+    this.toggleView = this.toggleView.bind(this);
+    this.toggleInstrument=this.toggleInstrument.bind(this);
   }
 
   toggleInstrument(inst) {
     this.setState({
-      instrument: inst
+      instrument: inst,
     });
   }
 
   toggleView(viewChange) {
     this.setState({
-      view: viewChange
+      view: viewChange,
     });
   }
 
   toggleRoom(roomType, RoomId) {
     this.setState({
-      roomType: roomType,
+      roomType,
       RoomId: RoomId?this.state.RoomId:RoomId,
-      view: "selectInstrument"
+      view: "selectInstrument",
     });
   }
 
   render() {
-    if (this.state.view==='LandingPage') {
-      return (
-        <div>
-          <Nav
-            change={this.toggleView}
-          />
-          <LandingPage
-            change={this.toggleView}
-          />
-        </div>
-      );
-    } else if (this.state.view==='login') {
-      return (
-        <div>
-          <Login
-            change={this.toggleView}
-          />
-        </div>
-      );
-    } else if (this.state.view==='signup') {
-      return (
-        <div>
-          <Signup
-            change={this.toggleView}
-          />
-        </div>
-      );
-    } else if (this.state.view==='selectInstrument') {
-      return (
-        <div>
+    return (
+      <div>
+        {this.state.view === 'LandingPage' ?
+          <div>
+            <Nav change={this.toggleView} />
+            <LandingPage change={this.toggleView} />
+          </div>
+          : null
+        }
+        {this.state.view === 'login' ?
+          <Login change={this.toggleView} />
+          : null
+        }
+        {this.state.view === 'signup' ?
+          <Signup change={this.toggleView} />
+          : null
+        }
+        {this.state.view==='selectInstrument' ?
           <SelectInstrument
             sel={this.toggleInstrument}
             inst={this.state.instrument}
             change={this.toggleView}
           />
-        </div>
-      );
-    } else if (this.state.view==='SelectRoom') {
-      return (
-        <div>
-          <SelectRoom
-            rooms={this.toggleRoom}
-          />
-        </div>)
-    } else if (this.state.view==='JamRoom') {
-      return (
-        <div>
+          : null
+        }
+        {this.state.view==='SelectRoom' ?
+          <SelectRoom rooms={this.toggleRoom} />
+          : null
+        }
+        {this.state.view==='JamRoom' ?
           <JamRoom
             inst={this.state.instrument}
           />
-        </div>
-      );
-    }
+          : null
+        }
+      </div>
+    );
   }
 }
 export default App;
