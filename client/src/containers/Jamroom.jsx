d1d0fe6d3a56496c4f6ca8ee3931c3f1af6d2234
Greg Roche <grroche.us@gmail.com>
2016-08-11 12:16:28 -0700

    Fix indentation of jsx

--- client/src/containers/Jamroom.jsx
+++ client/src/containers/Jamroom.jsx
@@ -2,11 +2,10 @@ import React, { Component } from 'react';
 import UserInstrument from './UserInstrument';
 
 class JamRoom extends Component {
-
   render() {
     return (
       <div id="jamroom">
-      <h1>Welcome to the JamRoom!</h1>
+        <h1>Welcome to the JamRoom!</h1>
         <UserInstrument />
       </div>
     );
