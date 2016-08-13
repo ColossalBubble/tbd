967b9dffee7b871bd815f02c51226aa3ee0c9a6c
Rafael Espinoza <nerddiffer@gmail.com>
2016-08-13 15:47:36 -0700

    Sort of implement Metronome

--- client/src/components/Metronome/index.jsx
+++ client/src/components/Metronome/index.jsx
@@ -1,64 +1,97 @@
 import React, { Component } from 'react';
 import Paper from 'material-ui/Paper';
 import Slider from 'material-ui/Slider';
-import { Loop, Transport } from 'tone';
+import { Loop, Transport, MembraneSynth } from 'tone';
+import StartStopButton from './StartStopButton';
+
+const beat = new MembraneSynth({
+  envelope: {
+    sustain: 0,
+    attack: 0.02,
+    decay: 0.8
+  },
+  octaves: 10
+}).toMaster();
+
+const kickOffBeat = (interval) => (
+  new Loop(time => {
+    beat.triggerAttackRelease('C2', '4n', time);
+  }, interval).start()
+);
 
 class Metronome extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      tempo: 60,
-      loop: null
-    };
-
-    this.changeTempo = this.changeTempo.bind(this);
-    this.tick = this.tick.bind(this);
-  }
-
-  changeTempo(_event, value) {
-    this.setState({ tempo: value });
-    this.tick(value);
-  }
-
-  tick(tempo) {
+    const bpm = 60;
     const interval = '4n'; // quarter note
 
-    const newLoop = new Loop(time => {
-      // console.log(time);
-    }, interval);
+    this.state = {
+      bpm,
+      interval,
+      transport: Transport.start(),
+      loop: kickOffBeat(interval)
+    };
 
-    const loop = this.state.loop;
-    // if metronome is active, then stop it
-    if (loop && loop.state === 'started') {
-      loop.cancel(0);
-      loop.dispose();
+    this.changeBPM = this.changeBPM.bind(this);
+    this.toggle = this.toggle.bind(this);
+    this.start = this.start.bind(this);
+    this.stop = this.stop.bind(this);
+  }
+
+  componentDidMount() {
+    const transport = this.state.transport;
+    console.log(transport.state);
+    transport.start();
+  }
+
+  start() {
+    console.log('pressed start');
+    this.state.transport.start();
+  }
+
+  stop() {
+    console.log('pressed stop');
+    this.state.transport.stop();
+  }
+
+  changeBPM(_event, value) {
+    this.setState({ bpm: value });
+    Transport.bpm.value = value;
+  }
+
+  toggle() {
+    const transport = this.state.transport;
+
+    console.log(transport.state);
+
+    if (transport.state !== 'started') {
+      this.start();
+    } else {
+      this.stop();
     }
-
-    this.setState({ loop: newLoop });
-
-    this.state.loop.start(0);
-    Transport.bpm.value = tempo;
-    Transport.start();
   }
 
   render() {
-    const tempo = this.state.tempo;
-    const changeTempo = this.changeTempo;
+    const bpm = this.state.bpm;
+    const changeBPM = this.changeBPM;
+    const toggle = this.toggle;
 
     return (
       <Paper>
         <h1>Metronome</h1>
         <Slider
-          defaultValue={tempo}
+          defaultValue={bpm}
           description="Change the tempo"
           min={40}
           max={240}
           step={2}
-          onChange={changeTempo}
+          onChange={changeBPM}
           value={61}
         />
-        Tempo: { tempo } bpm
+        Tempo: { bpm } bpm
+        <br />
+        <StartStopButton toggle={toggle} />
       </Paper>
     );
   }
