2823880c1631112d7973901a9448c4996054421a
Frances <franyang99@gmail.com>
2016-08-14 16:32:15 -0700

    Remove socket from room and emit offers and answers to specific socket

--- server/index.js
+++ server/index.js
@@ -28,11 +28,9 @@ io.on('connection', socket => {
 
   socket.on('join', room => {
     if (!rooms[room]) {
-      console.log('Not a valid room');
       // send socket message to user?
       io.to(socket.id).emit('invalid room');
     } else {
-      console.log('joining room', room);
       rooms[room] = rooms[room] || [];
 
       const numberOfClients = rooms[room].length;
@@ -46,21 +44,28 @@ io.on('connection', socket => {
 
         socket.on('disconnect', () => {
           const socketsInRoom = rooms[room];
-          socketsInRoom.splice(socketsInRoom.indexOf(socket.id.slice(2)), 1);
-          console.log('disconnecting', socketsInRoom, socket.id);
-          socket.leave(room);
+          const index = socketsInRoom.indexOf(socket.id.slice(2));
+          if (index > -1) {
+            socketsInRoom.splice(index, 1);
+            socket.leave(room);
+          }
         });
       }
     }
   });
 
-  // TODO: add to room for offer/answer emits
-  socket.on('offer', offer => {
-    socket.broadcast.emit('offer', offer);
+  socket.on('exit room', data => {
+    const room = rooms[data.room];
+    const index = room.indexOf(data.id);
+    room.splice(index, 1);
   });
 
-  socket.on('answer', data => {
-    socket.broadcast.emit('answer', data);
+  socket.on('offer', offer => {
+    socket.broadcast.to(`/#${offer.to}`).emit('offer', offer);
+  });
+
+  socket.on('answer', answer => {
+    socket.broadcast.to(`/#${answer.to}`).emit('answer', answer);
   });
 });
 
