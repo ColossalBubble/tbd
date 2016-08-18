95a4b567c7cb1193269f619f754d403ec3057734
Rafael Espinoza <nerddiffer@gmail.com>
2016-08-18 15:30:31 -0700

    Update styles
    - Remove unneeded stylesheet files to save on HTTP requests
    - Change positioning of NavBar to 'block', so it's in normal
      page flow (got to see the logo)
    - Center things

--- client/src/components/SelectInstrument/StartButton.jsx
+++ client/src/components/SelectInstrument/StartButton.jsx
@@ -2,15 +2,8 @@ import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 
 const StartButton = ({ handleStart, disabled }) => {
-  const buttonStyles = {
-    position: 'absolute',
-    left: '45%',
-    top: '50%'
-  };
-
   return (
     <RaisedButton
-     style={buttonStyles}
      label="Start"
      onClick={handleStart}
      disabled={disabled}
